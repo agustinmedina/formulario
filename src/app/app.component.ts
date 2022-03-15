41e3c800625492d532ec601af946ee296e8e0a51
agustinmedina <agustin_trompiz@yahoo.com.ar>
2022-03-15 17:25:26 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'formulario2';
+  public formLogin!: FormGroup;
+
+  constructor(private formBuilder:FormBuilder){
+
+  }
+
+  ngOnInit():void{
+    this.formLogin=this.formBuilder.group({
+      email:['',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: ['']
+
+    });
+  }
+  send():any {
+    console.log(this.formLogin.value);
+  }
+}
